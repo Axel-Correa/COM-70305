20ab999bf3378fc32ddf25fb974c13fa3053190e
Axel-Correa <163211476+Axel-Correa@users.noreply.github.com>
2024-10-20 21:17:17 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,4 +1,25 @@
-const moment =require("moment")
+import express from "express" 
+const PORT = 3000
+const app = express()
+
+app.get("/", (req,res) =>{
+    res.send(`<h1>Ccristales</h1>`)
+})
+app.get("/Productos", (req,res) =>{
+    res.send("<h2>Productos</h2>")
+})
+app.get("/ContactoCD", (req,res) =>{
+    res.send("<h2>Contacto</h2>")
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server online en el puerto ${PORT}`)
+})
+
+
+
+/*const moment =require("moment")
 let fechaActual = moment ()
 console.log (fechaActual)
 
@@ -7,5 +28,5 @@ if(!fechaCumpleaños.isValid()){
     console.log("fecha incorrecta")
     return
 }
-console.log()`usted ha nacido ${fechaActual.diff(fechaCumpleaños,"year")} años`
+console.log()`usted ha nacido ${fechaActual.diff(fechaCumpleaños,"year")} años`*/
 
